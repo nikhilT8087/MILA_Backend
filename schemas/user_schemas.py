a9330ee168a23ce74a4c9d4c8e12eed86d90a4bd
Mansi Kadam <Mansi K@PSPL.COM>
2025-12-02 11:11:30 +0530

    Removed boiler plate unwanted code

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -63,23 +63,9 @@ class RefreshTokenRequest(BaseModel):
     refresh_token: str
 
 
-#send_email Schema 
-# async def send_email(to_email: str, subject:str, body:str, is_html):  
-#     # Trigger the Celery task
-#     send_email_task.delay(to_email, subject, body)
-
-
 #LogoutRequest Schema 
 class LogoutRequest(BaseModel):
     refresh_token: str
-
-
-#ResetPasswordRequest Schema 
-# class ResetPasswordRequest(BaseModel):
-#     # email: str  # Add user_id here
-#     current_password: str
-#     new_password: str
-#     confirm_new_password: str
 
 
 # Request Reset Password Schema
@@ -114,97 +100,6 @@ class ForgotPasswordOtpVerify(BaseModel):
             raise ValueError("Email is required")
         return v.strip()
     
-
-#ForgotPasswordRequest Schema 
-# class ForgotPasswordRequest(BaseModel):
-#     reset_token: str
-#     new_password: str
-#     confirm_new_password: str
-
-#     @field_validator("reset_token")
-#     def validate_reset_token(cls, v):
-#         if not v or not v.strip():
-#             raise ValueError("Reset token cannot be empty.")
-#         return v.strip()
-
-#     @field_validator("new_password")
-#     def validate_new_password(cls, v):
-#         if not v or not v.strip():
-#             raise ValueError("New password cannot be empty.")
-#         return v
-
-#     @field_validator("confirm_new_password")
-#     def validate_confirm_new_password(cls, v, info):
-#         if not v or not v.strip():
-#             raise ValueError("Confirm password cannot be empty.")
-#         # Defer matching check to a root validator instead
-#         return v
-
-
-#AdminAccountCreateRequest Schema 
-class AdminAccountCreateRequest(BaseModel):
-    first_name: str = Field(..., max_length=50)
-    last_name: str = Field(..., max_length=50)
-    email: EmailStr
-    password: str
-    role: str = "admin"  # Default role is admin
-    is_verified: bool = True
-
-
-# Schema for the response with hashed password
-class AdminAccountResponse(BaseModel):
-    first_name: str
-    last_name: str
-    email: EmailStr
-    role: str
-    password: str
-    is_verified: bool = True
-
-    class Config:
-        from_attributes  = True
-
-
-#VerificationRequest Schema   
-class UsernameUpdateRequest(BaseModel):
-    username: Optional[str] = None
-    first_name: Optional[str] = Field(None, max_length=50)
-    last_name: Optional[str] = Field(None, max_length=50)
-    # s3_key: Optional[str] = None
-
-    @field_validator("first_name")
-    def validate_first_name(cls, v):
-        """Ensure first name is not empty, contains no numbers, and is properly trimmed."""
-        if not v or not v.strip():
-            raise ValueError("First name cannot be empty or contain only spaces.")
-        if any(char.isdigit() for char in v):
-            raise ValueError("First name cannot contain numbers.")
-        if len(v.strip()) < 2:
-            raise ValueError("First name must be at least 2 characters long.")
-        # Regex pattern: only letters, hyphens, apostrophes, and spaces allowed
-        if not re.match(r"^[a-zA-Z\s\-']+$", v):
-            raise ValueError("First name must contain only letters")
-        return v.strip()
-    
-    @field_validator("last_name")
-    def validate_last_name(cls, v):
-        """Ensure last name is not empty, contains no numbers, and is properly trimmed."""
-        if not v or not v.strip():
-            raise ValueError("Last name cannot be empty or contain only spaces.")
-        if any(char.isdigit() for char in v):
-            raise ValueError("Last name cannot contain numbers.")
-        if len(v.strip()) < 2:
-            raise ValueError("Last name must be at least 2 characters long.")
-        # Regex pattern: only letters, hyphens, apostrophes, and spaces allowed
-        if not re.match(r"^[a-zA-Z\s\-']+$", v):
-            raise ValueError("Last name must contain only letters")
-        return v.strip()
-
-    # @field_validator("s3_key")
-    # def validate_s3_key(cls, v):
-    #     """Ensure s3_key is not just spaces (if provided)."""
-    #     if v is not None and not v.strip():
-    #         raise ValueError("s3_key cannot be empty or contain only spaces.")
-    #     return v
 
 class Signup(BaseModel):
     username: str
